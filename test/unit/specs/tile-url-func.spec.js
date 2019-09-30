21760aa2bd220dc934c35fe4a83ae241e7c3dfc6
Vladimir Vershinin <ghettovoice@gmail.com>
2019-09-30 23:09:00 +0300

    Update tests and build configs

--- test/unit/specs/tile-url-func.spec.js
+++ test/unit/specs/tile-url-func.spec.js
@@ -1,12 +1,11 @@
-import 'ol/Map' // initialize basic projections
 import { createTileUrlFunction } from '../../../src'
 
 const urlTemplate = 'http://{a-c}.tileserver.org/{0z}/{x1}/{x2}/{x3}/{y1}/{y2}/{y3}.png'
 const flipUrlTemplate = 'http://{a-c}.tileserver.org/{0z}/{x1}/{x2}/{x3}/{-y1}/{-y2}/{-y3}.png'
 const tiles = [
-  [ [ 5, 160, -10 ], 'http://b.tileserver.org/05/000/000/160/000/000/009.png', 'http://b.tileserver.org/05/000/000/160/000/000/022.png' ],
-  [ [ 18, 6053278, -15687 ], 'http://b.tileserver.org/18/006/053/278/000/015/686.png', 'http://b.tileserver.org/18/006/053/278/000/246/457.png' ],
-  [ [ 10, 10907, -826 ], 'http://b.tileserver.org/10/000/010/907/000/000/825.png', 'http://b.tileserver.org/10/000/010/907/000/000/198.png' ],
+  [ [ 5, 160, 10 ], 'http://a.tileserver.org/05/000/000/160/000/000/010.png', 'http://a.tileserver.org/05/000/000/160/000/000/021.png' ],
+  [ [ 18, 6053278, 15687 ], 'http://b.tileserver.org/18/006/053/278/000/015/687.png', 'http://b.tileserver.org/18/006/053/278/000/246/456.png' ],
+  [ [ 10, 10907, 826 ], 'http://a.tileserver.org/10/000/010/907/000/000/826.png', 'http://a.tileserver.org/10/000/010/907/000/000/197.png' ],
 ]
 
 describe('ol-tilecache tests', function () {
